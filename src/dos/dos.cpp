df52924aa389c4f9c4c263f5c7b948a56164f351
ripsaw8080 <ripsaw8080@ed541006-0bf7-43e9-8c4d-6fc63c346d47>
2018-06-21 17:46:52 +0000

    Support some installer disk detection methods: block device count, and specific MBR/boot sector contents. Fixes (original) SimCity, Amberstar, and later MicroProse installers.
    Change-Id: I941e5117bf43e2a17ecd4760b0b8fe14014206d1
    git-svn-id: http://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4120 ed541006-0bf7-43e9-8c4d-6fc63c346d47

--- src/dos/dos.cpp
+++ src/dos/dos.cpp
@@ -843,15 +843,22 @@ static Bitu DOS_21Handler(void) {
 	case 0x51:					/* Get current PSP */
 		reg_bx = dos.psp();
 		break;
-	case 0x52: {				/* Get list of lists */
+	case 0x52:
+	{				/* Get list of lists */
+		Bit8u count=2; // floppy drives always counted
+		while (count < DOS_DRIVES && Drives[count] && !Drives[count]->isRemovable())
+			count++;
+		dos_infoblock.SetBlockDevices(count);
 		RealPt addr = dos_infoblock.GetPointer();
 		SegSet16(es, RealSeg(addr));
 		reg_bx = RealOff(addr);
-		LOG(LOG_DOSMISC, LOG_NORMAL)("Call is made for list of lists - let's hope for the best");
-		break; }
-			   //TODO Think hard how shit this is gonna be
-			   //And will any game ever use this :)
-	case 0x53:					/* Translate BIOS parameter block to drive parameter block */
+		LOG(LOG_DOSMISC, LOG_NORMAL)
+		("Call is made for list of lists - let's hope for the best");
+		break;
+	}
+	//TODO Think hard how shit this is gonna be
+            //And will any game ever use this :)
+        case 0x53:					/* Translate BIOS parameter block to drive parameter block */
 		E_Exit("Unhandled Dos 21 call %02X", reg_ah);
 		break;
 	case 0x54:					/* Get verify flag */
@@ -1181,14 +1188,20 @@ static Bitu DOS_27Handler(void) {
 }
 
 static Bitu DOS_25Handler(void) {
-	if (Drives[reg_al] == 0) {
+	if (reg_al >= DOS_DRIVES || !Drives[reg_al] || Drives[reg_al]->isRemovable()) {
 		reg_ax = 0x8002;
 		SETFLAGBIT(CF, true);
 	} else {
-		SETFLAGBIT(CF, false);
-		if ((reg_cx != 1) || (reg_dx != 1))
-			LOG(LOG_DOSMISC, LOG_NORMAL)("int 25 called but not as diskdetection drive %X", reg_al);
-
+		if (reg_cx == 1 && reg_dx == 0) {
+			if (reg_al >= 2) {
+				PhysPt ptr = PhysMake(SegValue(ds),reg_bx);
+				// write some BPB data into buffer for MicroProse installers
+				mem_writew(ptr+0x1c,0x3f); // hidden sectors
+			}
+		} else {
+			LOG(LOG_DOSMISC,LOG_NORMAL)("int 25 called but not as disk detection drive %u",reg_al);
+		}
+		SETFLAGBIT(CF,false);
 		reg_ax = 0;
 	}
 	SETFLAGBIT(IF, true);
@@ -1196,7 +1209,7 @@ static Bitu DOS_25Handler(void) {
 }
 static Bitu DOS_26Handler(void) {
 	LOG(LOG_DOSMISC, LOG_NORMAL)("int 26 called: hope for the best!");
-	if (Drives[reg_al] == 0) {
+	if (reg_al >= DOS_DRIVES || !Drives[reg_al] || Drives[reg_al]->isRemovable()) {	
 		reg_ax = 0x8002;
 		SETFLAGBIT(CF, true);
 	} else {
