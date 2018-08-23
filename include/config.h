571e667a8c8690ba25e459e82f577e920572d839
Frank Zhang <frankzhang123@gmail.com>
2018-08-23 22:29:09 +0800

    Add CMake support for building. (cherry picked from commit 4a6cdb77fb80b7cc5ed048bd44e5248d09574251)
    Change-Id: I2ef27d2945cfcf56c020bf4f9c74cf8e5fff7b42

---
+++ include/config.h
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (C) 2002-2017  The DOSBox Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#if C_ATTRIBUTE_ALWAYS_INLINE
+#define INLINE inline __attribute__((always_inline))
+#else
+#define INLINE inline
+#endif
+
+#if C_ATTRIBUTE_FASTCALL
+#define DB_FASTCALL __attribute__((fastcall))
+#else
+#define DB_FASTCALL
+#endif
+
+#if C_HAS_ATTRIBUTE
+#define GCC_ATTRIBUTE(x) __attribute__ ((x))
+#else
+#define GCC_ATTRIBUTE(x) /* attribute not supported */
+#endif
+
+#if C_HAS_BUILTIN_EXPECT
+#define GCC_UNLIKELY(x) __builtin_expect((x),0)
+#define GCC_LIKELY(x) __builtin_expect((x),1)
+#else
+#define GCC_UNLIKELY(x) (x)
+#define GCC_LIKELY(x) (x)
+#endif
+
+/* Version number of package */
+#define VERSION "SVN"
+
+#include <stdint.h>
+  typedef uint8_t   Bit8u;
+  typedef int8_t    Bit8s;
+  typedef uint16_t  Bit16u;
+  typedef int16_t   Bit16s;
+  typedef uint32_t  Bit32u;
+  typedef int32_t   Bit32s;
+  typedef uint64_t  Bit64u;
+  typedef int64_t   Bit64s;
+#if (__WORDSIZE == 64)
+  typedef Bit64u Bitu;
+  typedef Bit64s Bits;
+#else
+  typedef Bit32u Bitu;
+  typedef Bit32s Bits;
+#endif
